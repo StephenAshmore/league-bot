f32831a44567a06d1dbd7e47e761b957834b69ac
Stephen Ashmore <stephen@casestack.io>
2017-07-01 21:25:27 -0500

    Updates to initial structure.

--- bot/bot.py
+++ bot/bot.py
@@ -1,17 +1,19 @@
 from slackclient import SlackClient
 
 import logging
+import sys
+import os
 import re
 import time
 
 logger = logging.getLogger(__name__)
-API_TOKEN = 'xoxb-207584631110-cMz6V5ocdtfLADHPACufjC9I'
+slack_token = os.environ["SLACK_API_TOKEN"]
 
 
 class Bot(object):
     def __init__(self):
         self._client = SlackClient(
-            API_TOKEN,
+            slack_token,
         )
 
     def run(self):
@@ -21,12 +23,12 @@ class Bot(object):
             exit()
 
         self.channel_users = self._client.api_call("users.list")
+        self.bot_id = self. get_id_from_name("league_bot")
         while True:
             events = self._client.rtm_read()
             for event in events:
                 event_type = event.get('type')
                 if event_type == 'message':
-                    logger.info('Handling message.')
                     self.handle_message(event)
                 if (event_type == 'member_joined_channel ' or
                    event_type == 'member_left_channel'):
@@ -40,22 +42,38 @@ class Bot(object):
         if subtype == u'message_changed':
             return
 
-        if 'text' in event.keys() and event['text'].startswith('@league-bot'):
-            botname = 'league-bot'
+        if 'text' in event.keys() and event['text'].startswith('<@{}>'.format(self.bot_id)):
+            botname = 'league_bot'
             if 'user' in event.keys():
                 name = self.get_name_from_id(event['user'])
 
                 if name == botname or name == u'slackbot' or name is None:
                     return
 
+                # this is a message for leaguebot. Deal with message:
+                text = event['text']
+                reply = 'I\'m sorry, but I don\'t understand that command.'
+                commands = text.split(' ')
+                main_command = commands[0]
+                del commands[0]
+                if main_command == 'table':
+                    if self.current_league is not None:
+                        reply = self.current_league.show_table()
+
                 self._client.api_call(
                   "chat.postMessage",
                   channel=event['channel'],
-                  text='Hello from League Bot! @{} :tada:'.format(name)
+                  text=reply
                 )
 
     def get_name_from_id(self, id):
         for user in self.channel_users['members']:
             if id == user['id']:
                 return user['name']
+        return None
+
+    def get_id_from_name(self, name):
+        for user in self.channel_users['members']:
+            if name == user['name']:
+                return user['id']
         return None
